747c9995e51bb290733de9530b1b32def9bd6485
zhbit <zhbit@DESKTOP-UMDSFA0>
2016-12-19 08:23:37 +0800

    Core:Framework Build

---
+++ TemplatesHttp/TemplatesHttp/Adapters/AdapteSquence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplatesHttp.Core;
+
+namespace TemplatesHttp.Adapters
+{
+    public sealed class AdapteSquence : Queue<IStreamAdapter>
+    {
+
+    }
+}
